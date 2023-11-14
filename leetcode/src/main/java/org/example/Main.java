47a320712e534e81e15efee9efe47add2d1556a2
Charles Evans <charleslevans5@gmail.com>
2023-11-14 13:56:51 -0500

    two sum problem

---
+++ leetcode/src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Main {
+
+    // Two Sum (easy)
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++){
+            for (int j = i + 1; j < nums.length; j++){
+                if (nums[i] + nums[j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
+
+    }
+
+    
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
